f6f6bb8d0a4a281f4981bb30a4adadfbaecba9d7
ThomasPijper <56thomasp56@gmail.com>
2025-02-02 01:19:34 +0100

    Fix: Homescreen and navigation header are now responsive

--- src/app/components/navigation-header/navigation-header.component.ts
+++ src/app/components/navigation-header/navigation-header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, HostListener, Inject, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-navigation-header',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrl: './navigation-header.component.css'
 })
 export class NavigationHeaderComponent {
+  isSmallScreen: boolean = false;
 
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.isSmallScreen = window.innerWidth <= 600;
+    }
+  }
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      window.addEventListener('resize', this.onResize.bind(this));
+    }
+  }
+
+  onResize() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.isSmallScreen = window.innerWidth <= 600;
+    }
+  }
+
+  ngOnDestroy() {
+    if (isPlatformBrowser(this.platformId)) {
+      window.removeEventListener('resize', this.onResize.bind(this));
+    }
+  }
 }
